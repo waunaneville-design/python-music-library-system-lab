0b8b283e0b1114c9dac6c98c47510c63c1d7c72a
Lascelles <waunaneville@gmail.com>
2026-06-12 15:44:18 +0300

    Add class methods for artist and genre tracking - Implement add_to_artists(cls, artist) to maintain a list of unique artists - Implement add_to_genre_count(cls, genre) to increment per-genre song counts - Implement add_to_artist_count(cls, artist) to increment per-artist song counts - Together these methods build centralized statistics for all Song instances

--- lib/song.py
+++ lib/song.py
@@ -25,3 +25,19 @@ class Song:
         if genre not in cls.genres:
             cls.genres.append(genre)
 
+ @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre not in cls.genre_count:
+            cls.genre_count[genre] = 0
+        cls.genre_count[genre] += 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist not in cls.artist_count:
+            cls.artist_count[artist] = 0
+        cls.artist_count[artist] += 1
